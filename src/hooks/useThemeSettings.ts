781a53cfbd8b1f92db97a01dbdc0697600a5ac7c
samwyness <samwyness22@gmail.com>
2021-07-12 21:51:36 +1000

    feat(theme): add useThemeSettings hook

---
+++ src/hooks/useThemeSettings.ts
@@ -0,0 +1,25 @@
+import { useTheme } from 'native-base';
+import { ColorSchemeName, useColorScheme } from 'react-native';
+
+type ThemeSettings = {
+  colorScheme: ColorSchemeName;
+  isDarkMode: boolean;
+  statusBarStyle: 'light-content' | 'dark-content';
+  backgroundColor: string;
+  textColor: string;
+};
+
+export const useThemeSettings = (): ThemeSettings => {
+  const colorScheme = useColorScheme();
+  const theme = useTheme();
+  const isDarkMode = colorScheme === 'dark';
+  const statusBarScheme = isDarkMode ? 'light-content' : 'dark-content';
+
+  return {
+    colorScheme,
+    isDarkMode,
+    statusBarStyle: statusBarScheme,
+    backgroundColor: isDarkMode ? theme.colors.black : theme.colors.white,
+    textColor: isDarkMode ? theme.colors.lightText : theme.colors.darkText,
+  };
+};
