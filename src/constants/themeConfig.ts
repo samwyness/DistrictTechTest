517e7674eb8d0ea16dc9a34d548d0bb04b21d8da
samwyness <samwyness22@gmail.com>
2021-07-13 21:36:23 +1000

    feat(theme): add default and brand variants to Button component

--- src/constants/themeConfig.ts
+++ src/constants/themeConfig.ts
@@ -1,3 +1,5 @@
+import { Dict } from 'native-base/lib/typescript/theme/tools';
+import { Appearance } from 'react-native';
 
 const colors = {
   brand: {
@@ -6,6 +8,44 @@ const colors = {
     dark: '#bc0035',
   },
 };
+
+function buttonDefault(props: Dict) {
+  const isDarkMode = Appearance.getColorScheme() === 'dark';
+  let bg = isDarkMode ? 'dark.900' : 'dark.50';
+
+  if (props.isDisabled) {
+    bg = isDarkMode ? 'dark.500' : 'dark.300';
+  }
+
+  return {
+    _web: {
+      outlineWidth: 0,
+    },
+    bg,
+    _pressed: {
+      bg: isDarkMode ? 'dark.700' : 'dark.200',
+    },
+  };
+}
+
+function buttonBrand(props: Dict) {
+  const isDarkMode = Appearance.getColorScheme() === 'dark';
+  let bg = 'brand.primary';
+
+  if (props.isDisabled) {
+    bg = isDarkMode ? 'dark.500' : 'dark.300';
+  }
+
+  return {
+    _web: {
+      outlineWidth: 0,
+    },
+    bg,
+    _pressed: {
+      bg: 'brand.light',
+    },
+  };
+}
 
 export const themeConfig = {
   config: {
@@ -21,6 +61,14 @@ export const themeConfig = {
         _text: {
           textTransform: 'uppercase',
         },
+      },
+      defaultProps: {
+        variant: 'default',
+        colorScheme: 'default',
+      },
+      variants: {
+        default: buttonDefault,
+        brand: buttonBrand,
       },
     },
   },
