2e91df2ed1ff1fee6d775ba1d29d6fd00233b1eb
samwyness <samwyness22@gmail.com>
2021-07-14 17:24:21 +1000

    feat(navigation): add Restaurants and Map tabs

---
+++ src/navigation/RestaurantsTab.navigator.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import { screens } from './config';
+
+const Stack = createStackNavigator();
+
+const RestaurantsTab = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name={screens.Restaurants.name}
+        component={screens.Restaurants.component}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default RestaurantsTab;
