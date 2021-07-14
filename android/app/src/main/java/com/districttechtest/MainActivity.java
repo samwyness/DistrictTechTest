2e57f056bfde21953aece848829f6dfc5b2411a1
samwyness <samwyness22@gmail.com>
2021-07-14 23:36:14 +1000

    build(android): set FLAG_KEEP_SCREEN_ON

--- android/app/src/main/java/com/districttechtest/MainActivity.java
+++ android/app/src/main/java/com/districttechtest/MainActivity.java
@@ -1,8 +1,16 @@
 package com.districttechtest;
 
+import android.os.Bundle;
+import android.view.WindowManager;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    this.getWindow()
+      .addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
