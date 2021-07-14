179bd785ef86f8e2e14080e212c3527b83b3f1dc
samwyness <samwyness22@gmail.com>
2021-07-14 23:33:01 +1000

    feat(app): add RestaurantsProvider to App

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,19 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { NativeBaseProvider, extendTheme } from 'native-base';
 import { StatusBar } from 'react-native';
+
 import { themeConfig } from './constants/themeConfig';
+import { RestaurantsProvider } from './contexts/RestaurantsContext';
 import Navigation from './navigation';
 
 const App = () => {
   return (
-    <NativeBaseProvider theme={extendTheme(themeConfig)}>
-      <StatusBar />
-      <Navigation />
-    </NativeBaseProvider>
+    <RestaurantsProvider>
+      <NativeBaseProvider theme={extendTheme(themeConfig)}>
+        <StatusBar />
+        <Navigation />
+      </NativeBaseProvider>
+    </RestaurantsProvider>
   );
 };
 
