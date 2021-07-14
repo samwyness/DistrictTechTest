533f8a03fd384c02ea3a29d4fcb062c3827da7f0
samwyness <samwyness22@gmail.com>
2021-07-14 23:31:30 +1000

    feat(hooks): create useCurrentLocation hook

---
+++ src/hooks/useCurrentLocation.ts
@@ -0,0 +1,26 @@
+import { useCallback, useEffect, useState } from 'react';
+import { LatLng } from 'react-native-maps';
+import Geolocation from '@react-native-community/geolocation';
+
+export const useCurrentLocation = () => {
+  const [currentLocation, setCurrentLocation] = useState<LatLng>();
+
+  const getCurrentLocation = useCallback(() => {
+    Geolocation.getCurrentPosition(position => {
+      setCurrentLocation({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      });
+    });
+  }, []);
+
+  // Determine current location on first render
+  useEffect(() => {
+    getCurrentLocation();
+  }, [getCurrentLocation]);
+
+  return {
+    currentLocation,
+    getCurrentLocation,
+  };
+};
