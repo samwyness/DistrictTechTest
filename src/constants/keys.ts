216f9d2bd344b4e5eaa82a4a3f89eb92fc50b4a0
samwyness <samwyness22@gmail.com>
2021-07-14 23:26:33 +1000

    feat(constants): add temp keys

---
+++ src/constants/keys.ts
@@ -0,0 +1,4 @@
+// TODO: API Keys should be stored in a build process.
+// For the purposes of this test, these keys are exposed here and will eventually be invalidated
+
+export const GOOGLE_MAPS_API_KEY = 'AIzaSyDUtvvygQjJ14B9Za47wCQHuK38OodF7sw';
