5b380b0ee8cb7a04c7d8082ebb265c3f9f9c7f5b
Sam Wyness <samwyness22@gmail.com>
2021-07-15 18:31:37 +1000

    feat(screens): add restaurant cards to RestaurantsScreen

--- src/screens/RestaurantsScreen.tsx
+++ src/screens/RestaurantsScreen.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
-import { View } from 'native-base';
+import { FlatList } from 'native-base';
 
 import { useThemeSettings } from '../hooks/useThemeSettings';
+import { useRestaurants } from '../hooks/useRestaurants';
+import { Restaurant } from '../models/Restaurant';
+import RestaurantCard from '../components/RestaurantCard';
 
 const RestaurantsScreen = () => {
   const themeSettings = useThemeSettings();
+  const { restaurants } = useRestaurants();
 
-  return <View flex={1} backgroundColor={themeSettings.backgroundColor} />;
+  return (
+    <FlatList
+      data={restaurants}
+      backgroundColor={themeSettings.backgroundColor}
+      keyExtractor={item => (item as Restaurant).id}
+      renderItem={(result: { item: Restaurant }) => (
+        <RestaurantCard restaurant={result.item} />
+      )}
+    />
+  );
 };
 
 export default RestaurantsScreen;
