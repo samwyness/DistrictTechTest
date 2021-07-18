08742d44ff0952bd9359327c03a92034978c4779
Sam Wyness <samwyness22@gmail.com>
2021-07-18 18:02:13 +1000

    feat(services): add getPlaceDetailsByReference and getPlacePhotoByReference methodes; clean up models to match changes

--- src/services/RestaurantService.ts
+++ src/services/RestaurantService.ts
@@ -1,13 +1,16 @@
 import { GOOGLE_MAPS_API_KEY } from '../constants/keys';
 import { Restaurant } from '../models/Restaurant';
 
-export type GooglePlaceResult = {
+export interface GooglePlaceResult {
   place_id: string;
   name: string;
+  types: string[];
   rating: number;
   user_ratings_total: number;
   vicinity: string;
-  opening_hours?: {
+  formatted_phone_number?: string;
+  website?: string;
+  opening_hours: {
     open_now: boolean;
   };
   photos?: {
@@ -21,10 +24,14 @@ export type GooglePlaceResult = {
       lng: number;
     };
   };
-};
+}
 
 export type NearbyRestaurantsResponse = {
   results: GooglePlaceResult[];
+};
+
+export type RestaurantDetailResponse = {
+  result: GooglePlaceResult;
 };
 
 export default class RestaurantService {
@@ -35,10 +42,10 @@ export default class RestaurantService {
   ): Promise<Restaurant[]> {
     const baseUrl =
       'https://maps.googleapis.com/maps/api/place/nearbysearch/json?';
-
     const searchParams = new URLSearchParams({
-      location: `${latitude},${longitude}&radius=${radius}`,
       types: 'restaurant',
+      location: `${latitude},${longitude}`,
+      radius: `${radius}`,
       key: GOOGLE_MAPS_API_KEY,
     });
 
@@ -46,7 +53,45 @@ export default class RestaurantService {
       const response = await fetch(baseUrl + searchParams);
       const json = (await response.json()) as NearbyRestaurantsResponse;
 
-      return json.results.map(result => this.googlePlaceToRestaurant(result));
+      return json.results.map(place => this.googlePlaceToRestaurant(place));
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  async getPlaceDetailsByReference(placeId: string): Promise<Restaurant> {
+    const baseUrl = ' https://maps.googleapis.com/maps/api/place/details/json?';
+    const searchParams = new URLSearchParams({
+      place_id: placeId,
+      fields:
+        'place_id,name,vicinity,formatted_phone_number,website,rating,user_ratings_total,opening_hours,photos,geometry',
+      key: GOOGLE_MAPS_API_KEY,
+    });
+
+    try {
+      const response = await fetch(baseUrl + searchParams);
+      const json = (await response.json()) as RestaurantDetailResponse;
+
+      return this.googlePlaceToRestaurant(json.result);
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  async getPlacePhotoByReference(
+    photoRef: string,
+    maxWidth: string,
+  ): Promise<string | null> {
+    const baseUrl = ' https://maps.googleapis.com/maps/api/place/photo?';
+    const searchParams = new URLSearchParams({
+      photoreference: photoRef,
+      maxwidth: maxWidth,
+      key: GOOGLE_MAPS_API_KEY,
+    });
+
+    try {
+      const response = await fetch(baseUrl + searchParams);
+      return response.url;
     } catch (error) {
       throw new Error(error);
     }
@@ -56,17 +101,16 @@ export default class RestaurantService {
     return {
       id: place.place_id,
       name: place.name,
+      types: place.types,
       isOpen: place.opening_hours?.open_now || false,
       rating: place.rating,
       totalRatings: place.user_ratings_total,
       address: place.vicinity,
-      image:
+      phoneNumber: place.formatted_phone_number || null,
+      website: place.website || null,
+      imageRef:
         place.photos && place.photos.length > 0
-          ? {
-              src: place.photos[0].photo_reference,
-              width: place.photos[0].width,
-              height: place.photos[0].height,
-            }
+          ? place.photos[0].photo_reference
           : null,
       marker: {
         latitude: place.geometry.location.lat,
