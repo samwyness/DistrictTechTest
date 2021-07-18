91623ae0122548e0ab0ebb76b7cef3814a322f59
Sam Wyness <samwyness22@gmail.com>
2021-07-18 17:58:18 +1000

    fix(hooks): add permission check before accessing location data

--- src/hooks/useCurrentLocation.ts
+++ src/hooks/useCurrentLocation.ts
@@ -1,26 +1,57 @@
 import { useCallback, useEffect, useState } from 'react';
+import { PermissionsAndroid } from 'react-native';
 import { LatLng } from 'react-native-maps';
 import Geolocation from '@react-native-community/geolocation';
 
 export const useCurrentLocation = () => {
+  const [hasLocationPermission, setHasLocationPermission] = useState(false);
   const [currentLocation, setCurrentLocation] = useState<LatLng>();
 
-  const getCurrentLocation = useCallback(() => {
-    Geolocation.getCurrentPosition(position => {
-      setCurrentLocation({
-        latitude: position.coords.latitude,
-        longitude: position.coords.longitude,
-      });
-    });
+  const requestLocationPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      );
+
+      console.log(
+        'DEBUG: PERMISSIONS.ACCESS_FINE_LOCATION',
+        PermissionsAndroid.RESULTS.GRANTED,
+      );
+
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        setHasLocationPermission(true);
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+
+  const getCurrentLocation = useCallback(async () => {
+    Geolocation.getCurrentPosition(
+      position => {
+        setCurrentLocation({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        });
+      },
+      error => {
+        console.log(error);
+      },
+    );
   }, []);
 
   // Determine current location on first render
   useEffect(() => {
-    !currentLocation && getCurrentLocation();
-  }, [currentLocation, getCurrentLocation]);
+    if (hasLocationPermission && !currentLocation) {
+      getCurrentLocation();
+    } else {
+      requestLocationPermission();
+    }
+  }, [currentLocation, getCurrentLocation, hasLocationPermission]);
 
   return {
     currentLocation,
+    hasLocationPermission,
     getCurrentLocation,
   };
 };
