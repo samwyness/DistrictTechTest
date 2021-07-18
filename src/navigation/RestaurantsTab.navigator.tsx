aa04675db2305899d23535cc82101f2ce23003a4
Sam Wyness <samwyness22@gmail.com>
2021-07-18 18:05:17 +1000

    chore: update config path references

--- src/navigation/RestaurantsTab.navigator.tsx
+++ src/navigation/RestaurantsTab.navigator.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import { screens } from './config';
+import { screens } from '../config/navigation';
 
 const Stack = createStackNavigator();
 
