bbe713b3973a85d72b3f7b97e3c731613e31cca0
Sam Wyness <samwyness22@gmail.com>
2021-07-19 12:34:40 +1000

    feat(components): show restaurant details screen when MapMarker callout is pressed

--- src/components/MapMarker/MapMarker.component.tsx
+++ src/components/MapMarker/MapMarker.component.tsx
@@ -1,28 +1,59 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Center, Box } from 'native-base';
 import { LatLng, Marker } from 'react-native-maps';
+import { useNavigation } from '@react-navigation/native';
+
+import { RestaurantDetailScreenNavigationProp } from '../../screens/RestaurantDetailScreen';
+import { useRef } from 'react';
+import { useEffect } from 'react';
 
 type MapMarkerProps = {
   coordinates: LatLng;
   title: string;
+  restaurantId: string;
+  isFocused?: boolean;
 };
 
-const MapMarker: React.FC<MapMarkerProps> = ({ coordinates, title }) => {
+const MapMarker: React.FC<MapMarkerProps> = ({
+  coordinates,
+  title,
+  restaurantId,
+  isFocused = false,
+}) => {
+  const marker = useRef<Marker>(null);
+  const navigation = useNavigation<RestaurantDetailScreenNavigationProp>();
+
+  const handleOnPress = useCallback(() => {
+    navigation.navigate('RestaurantDetail', {
+      restaurantId: restaurantId,
+    });
+  }, [navigation, restaurantId]);
+
+  useEffect(() => {
+    if (isFocused) {
+      marker.current?.showCallout();
+    }
+  }, [isFocused]);
+
   return (
-    <Marker coordinate={coordinates} title={title}>
+    <Marker
+      ref={marker}
+      coordinate={coordinates}
+      title={title}
+      onCalloutPress={handleOnPress}>
       <Center>
         <Box
-          width={8}
-          height={8}
+          width={6}
+          height={6}
           backgroundColor="brand.opaque"
           borderRadius="full">
           <Box
             pos="absolute"
             top={1}
             left={1}
-            width={6}
-            height={6}
-            backgroundColor="brand.opaque"
+            width={4}
+            height={4}
+            backgroundColor="brand.primary"
             borderRadius="full"
           />
         </Box>
