d177651eab34896a379e13676527e47f9673e2a6
Sam Wyness <samwyness22@gmail.com>
2021-07-19 12:38:02 +1000

    feat(components): update RestaurantMap to handle focused places passed in via props

--- src/components/RestaurantMap/RestaurantMap.component.tsx
+++ src/components/RestaurantMap/RestaurantMap.component.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { View } from 'native-base';
+import { Box, Button, View } from 'native-base';
 import MapView, { PROVIDER_GOOGLE, Region } from 'react-native-maps';
 
 import { defaultMapStyle, darkMapStyle } from '../../config/googleMapsConfig';
@@ -8,14 +8,22 @@ import { useThemeSettings } from '../../hooks/useThemeSettings';
 import MapMarker from '../MapMarker';
 import { styles } from './RestaurantMap.style';
 import { Restaurant } from '../../models/Restaurant';
+import { useRef } from 'react';
 
 type RestaurantMapProps = {
   restaurants: Restaurant[];
+  restaurantId: string | null;
 };
 
-const RestaurantMap: React.FC<RestaurantMapProps> = ({ restaurants }) => {
+const RestaurantMap: React.FC<RestaurantMapProps> = ({
+  restaurants,
+  restaurantId,
+}) => {
+  const map = useRef<MapView>(null);
   const { currentLocation } = useCurrentLocation();
   const [mapRegion, setMapRegion] = useState<Region>();
+  const [showMapRefresh, setShowMapRefresh] = useState(false);
+  const [focusPlace, setFocusPlace] = useState<Restaurant | null>(null);
 
   const googleMapsStyle = useThemeSettings().isDarkMode
     ? darkMapStyle
@@ -23,30 +31,79 @@ const RestaurantMap: React.FC<RestaurantMapProps> = ({ restaurants }) => {
 
   const handleOnRegionChangeComplete = useCallback((region: Region) => {
     setMapRegion(region);
+    setShowMapRefresh(true);
+  }, []);
+
+  const handleOnPressRefreshMap = useCallback(() => {
+    setShowMapRefresh(false);
+  }, []);
+
+  const handleOnMarkerDeselect = useCallback(() => {
+    setFocusPlace(null);
   }, []);
 
   // Set map region to current location
   useEffect(() => {
-    !mapRegion &&
-      currentLocation &&
+    if (!mapRegion && currentLocation) {
       setMapRegion({
         latitude: currentLocation.latitude,
         longitude: currentLocation.longitude,
         latitudeDelta: 0.015, // TODO: calculate values based on viewport
         longitudeDelta: 0.0121, // TODO: calculate values based on viewport
       });
+    }
   }, [currentLocation, mapRegion]);
+
+  // Update focusPlace when restaurantId props changes
+  useEffect(() => {
+    if (restaurantId) {
+      const tempPlace =
+        restaurants.find(item => item.id === restaurantId) || null;
+      tempPlace && setFocusPlace(tempPlace);
+    }
+  }, [restaurantId, restaurants]);
+
+  // Animate to focusPlace marker
+  // Note: Stop clashes with 'setMapRegion' by checking for 'MapRegion' first
+  useEffect(() => {
+    if (focusPlace && mapRegion) {
+      const newRegion: Region = {
+        latitude: focusPlace.marker.latitude,
+        longitude: focusPlace.marker.longitude,
+        latitudeDelta: 0.015, // TODO: calculate values based on viewport
+        longitudeDelta: 0.0121, // TODO: calculate values based on viewport
+      };
+
+      map.current?.animateToRegion(newRegion);
+    }
+  }, [focusPlace, mapRegion]);
 
   return (
     <View style={styles.container}>
+      {!focusPlace && showMapRefresh && (
+        <Box
+          flex={1}
+          justifyContent="center"
+          position="absolute"
+          top={4}
+          zIndex={10}>
+          <Button size="sm" shadow={4} onPress={handleOnPressRefreshMap}>
+            Search this area
+          </Button>
+        </Box>
+      )}
+
       {mapRegion && (
         <MapView
+          ref={map}
+          loadingEnabled
           provider={PROVIDER_GOOGLE} // Required for Google Maps
           style={styles.map}
           initialRegion={mapRegion}
           region={mapRegion}
           customMapStyle={googleMapsStyle}
-          onRegionChangeComplete={handleOnRegionChangeComplete}>
+          onRegionChangeComplete={handleOnRegionChangeComplete}
+          onMarkerDeselect={handleOnMarkerDeselect}>
           {restaurants.map((item, index) => (
             <MapMarker
               key={index}
@@ -55,6 +112,8 @@ const RestaurantMap: React.FC<RestaurantMapProps> = ({ restaurants }) => {
                 longitude: item.marker.longitude,
               }}
               title={item.name}
+              restaurantId={item.id}
+              isFocused={item.id === focusPlace?.id}
             />
           ))}
         </MapView>
--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -1,17 +1,33 @@
 import React from 'react';
 import { View } from 'native-base';
+import { RouteProp } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
 
 import { useThemeSettings } from '../hooks/useThemeSettings';
 import { useRestaurants } from '../hooks/useRestaurants';
+import { AppBottomBarParamList } from '../navigation/AppBarBottom.navigator';
 import RestaurantMap from '../components/RestaurantMap';
 
-const MapScreen = () => {
+export type MapScreenNavigationProp = StackNavigationProp<
+  AppBottomBarParamList,
+  'Map'
+>;
+
+export type MapScreenProps = {
+  route: RouteProp<AppBottomBarParamList, 'Map'>;
+  navigation: MapScreenNavigationProp;
+};
+
+const MapScreen: React.FC<MapScreenProps> = ({ route }) => {
   const themeSettings = useThemeSettings();
   const { restaurants } = useRestaurants();
 
   return (
     <View flex={1} backgroundColor={themeSettings.backgroundColor}>
-      <RestaurantMap restaurants={restaurants} />
+      <RestaurantMap
+        restaurants={restaurants}
+        restaurantId={route.params.restaurantId}
+      />
     </View>
   );
 };
