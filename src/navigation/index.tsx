1b50f40e7475f68770301a731ca2c7b30cc25362
Sam Wyness <samwyness22@gmail.com>
2021-07-19 12:32:40 +1000

    feat(navigation): show loading screen while waiting for inital restaurant data to load

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -5,30 +5,23 @@ import {
   DarkTheme,
   NavigationContainerRef,
 } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 
-import { screens } from '../config/navigation';
 import { useThemeSettings } from '../hooks/useThemeSettings';
 import AppBarBottomNavigator from './AppBarBottom.navigator';
-
-export type RootStackParamList = {
-  Root: undefined;
-  RestaurantDetailScreen: {
-    restaurantId: string;
-  };
-};
-
-const Stack = createStackNavigator<RootStackParamList>();
+import LoadingScreen from '../screens/LoadingScreen';
+import { useRestaurants } from '../hooks/useRestaurants';
 
 const Navigation: React.FC = () => {
   const themeSettings = useThemeSettings();
+  const { isLoading, restaurants } = useRestaurants();
   const navigationRef = useRef<NavigationContainerRef>(null);
   const [currentRouteName, setCurrentRouteName] = useState<string>();
 
   const updateRouteName = useCallback(() => {
     const routeName = navigationRef.current?.getCurrentRoute()?.name;
+
     if (routeName) {
-      console.log('DEBUG: Navigation', routeName);
+      // console.log(`DEBUG: routeName=${routeName}`);
       setCurrentRouteName(routeName);
     }
   }, []);
@@ -37,15 +30,12 @@ const Navigation: React.FC = () => {
     <NavigationContainer
       ref={navigationRef}
       theme={themeSettings.isDarkMode ? DarkTheme : DefaultTheme}
-      onReady={updateRouteName}
       onStateChange={updateRouteName}>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Root" component={AppBarBottomNavigator} />
-        <Stack.Screen
-          name={screens.RestaurantDetail.name as 'RestaurantDetailScreen'}
-          component={screens.RestaurantDetail.component}
-        />
-      </Stack.Navigator>
+      {!isLoading && restaurants.length > 0 ? (
+        <AppBarBottomNavigator />
+      ) : (
+        <LoadingScreen />
+      )}
     </NavigationContainer>
   );
 };
